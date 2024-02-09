3376612ece7141bb38ec43939f6a69bd4c92aa74
Paula Cano <paulacanoroman@gmail.com>
2024-02-09 21:20:15 +0100

    refactor folder counter

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
-import Counter from "@/components/counter";
+import Counter from "@/components/counter/Counter";
 
 
 export default function Home() {
