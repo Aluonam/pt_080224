b5b206ebbbdc54de052cf59ff30e22b9bc691ba4
Paula Cano <paulacanoroman@gmail.com>
2024-02-10 20:38:28 +0100

    feat: folder and component create and import session

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import Counter from "@/components/counter/Counter";
+import { Session } from "@/components/session/Session";
 
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
       </Head>
       <main className={`${styles.main}`}>
         <Counter></Counter>
+        <Session></Session>
       </main>
     </>
   );
