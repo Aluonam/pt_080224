e994acf128e6255dd2010db1c86e4e6ac49de44d
Paula Cano <paulacanoroman@gmail.com>
2024-02-10 20:55:34 +0100

    feat: skeleton inputs

--- components/session/Session.jsx
+++ components/session/Session.jsx
@@ -2,11 +2,16 @@ import React, { useState } from 'react'
 
 export const Session = () => {
 
-    const [user, setUser] = uUeState({
+    const [user, setUser] = useState({
         userName: '',
         userPass: ''
     })
   return (
-    <div>Session</div>
+    <div>Session
+        <h3>Name:</h3>
+        <input type='text'></input>
+        <h3>Password:</h3>
+        <input type='password'></input>
+    </div>
   )
 }
