71fe42ed706c225b4f6f47c7504a25d9db804078
Paula Cano <paulacanoroman@gmail.com>
2024-02-12 22:58:18 +0100

    generics

--- components/session/Session.jsx
+++ components/session/Session.jsx
@@ -3,17 +3,21 @@ import React, { useState } from 'react'
 export const Session = () => {
 
     const [user, setUser] = useState({
-        userName: 'manoli',
-        userPass: '123'
+        userName: '',
+        userPass: ''
     })
+
+    const sendData = () => {
+        const newArr = structuredClone(user)
+        console.log(newArr)
+    }
   return (
     <div>Session
-        
         <h3>Name:</h3>
         <input type='text' value={user.userName} onChange={(e)=>{setUser({...user, name: e.target.value})}}></input>
         <h3>Password:</h3>
         <input type='password' value={user.userPass} onChange={(e)=>{setUser({...user, pass: e.target.value})}}></input>
-
+        <button onClick={()=>{sendData()}}>Send</button>
     </div>
   )
 }
